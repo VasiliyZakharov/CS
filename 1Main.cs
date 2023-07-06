8f070f16003ceb024b977d887a191956967d54c8
VasiliyZakharov <119455484+VasiliyZakharov@users.noreply.github.com>
2023-07-06 19:13:13 +0300

    Create 1Main.cs

---
+++ 1Main.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEquatable<Student> student1 = new Student("Vasya", "Zakharov", "Anatolevich", "M8O-208B-21", "c#");
+            Student student2 = new Student("Vasya", "Zakharov", "Anatolevich", "M8O-208B-21", "c#");
+            Console.WriteLine(student1.ToString());
+            Console.WriteLine(student1.Equals(student2));
+            Console.ReadKey();
+        }
+    }
+}
