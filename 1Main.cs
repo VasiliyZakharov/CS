38b8f40b595fe4f23635b19930553e6ce8ac4cd0
VasiliyZakharov <119455484+VasiliyZakharov@users.noreply.github.com>
2023-07-06 20:18:30 +0300

    Update 1Main.cs

--- 1Main.cs
+++ 1Main.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             IEquatable<Student> student1 = new Student("Vasya", "Zakharov", "Anatolevich", "M8O-208B-21", "c#");
             Student student2 = new Student("Vasya", "Zakharov", "Anatolevich", "M8O-208B-21", "c#");
+            Console.WriteLine(student2.YearOfEducation());
             Console.WriteLine(student1.ToString());
             Console.WriteLine(student1.Equals(student2));
             Console.ReadKey();
